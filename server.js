9a956ac4eb551a2a5cebf5227cf3301ac5d0f4ba
JonBay <134355923+JonBay@users.noreply.github.com>
2023-11-22 23:21:03 -0700

    changes to port

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+//const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3000;
+
 
 const app = express();
 
@@ -27,6 +29,14 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.listen(PORT, () =>
+//app.listen(PORT, () =>
+// console.log(`App listening at http://localhost:${PORT}`)
+//);
+
+
+
+// Listen on `port` and 0.0.0.0
+app.listen(PORT, "0.0.0.0", function () {
   console.log(`App listening at http://localhost:${PORT}`)
-);
+});
+
